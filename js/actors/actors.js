b611324bad31a421a503c34e415c716f636938a3
Roman Melnyk <email.rom.melnyk@gmail.com>
2015-06-28 18:55:31 +0200

    Actors description

--- js/actors/actors.js
+++ js/actors/actors.js
@@ -1,3 +1,10 @@
 (function (GAME) {
+	// ---------------------------
+	// Each Actor should have following methods:
+	//     - start ()				starts the actor's impact on the GAME.Model
+	//     - pause ()				pauses activity; actor should not impact the GAME.Model
+	//     - isActive ()			returns boolean value
+	//     - winsWhen ()			returns `true` when the actor wins according the scenario
+	// ---------------------------
 	GAME.Actors = {};
 })(GAME);
